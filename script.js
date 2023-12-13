710efb8c0e4d62b2033ad075b73b23b9992b2c01
otman <otman@DESKTOP-4RA473J.localdomain>
2023-12-13 11:28:28 +0100

    projet V1 by otman

---
+++ script.js
@@ -0,0 +1,120 @@
+
+const collection=[
+    {
+        name: "Lionel Messi",
+        nationalite:"argentin",
+        poste: "attaquant",
+        birth : 1987,
+        picture: "/images/messi.jpg",
+        taille: 170,
+        poids: 72,
+    }, 
+    
+    {
+        name: "Samuel Eto'o",
+        nationalite: "camerounais",
+        poste: "attaquant",
+        birth : 1981,
+        picture: "/images/etoo.jpg",
+        taille: 180,
+        poids: 75,
+    },
+
+    {
+        name: "Thierry Henry",
+        nationalite: "francais",
+        poste: "attaquant",
+        birth : 1977,
+        picture: "/images/henry.jpg",
+        taille: 188,
+        poids: 83,
+    },
+
+    {
+        name: "Andres Iniesta",
+        nationalite: "espagnol",
+        poste: "milieu",
+        birth : 1984,
+        picture: "/images/iniesta.jpg",
+        taille: 171,
+        poids: 68,
+    },
+
+    {
+        name: "Xavi",
+        nationalite: "espagnol",
+        poste: "milieu",
+        birth : 1980,
+        picture: "/images/xavi.jpeg",
+        taille: 170,
+        poids: 68,
+    },
+
+    {
+        name: "Sergio Busquets",
+        nationalite: "espagnol",
+        poste: "milieu",
+        birth : 1980,
+        picture: "/images/sergio.jpg",
+        taille: 189,
+        poids: 76,
+    },
+
+    {
+        name: "Carles Puyol",
+        nationalite: "espagnol",
+        poste: "defenseur",
+        birth : 1978,
+        picture: "/images/puyol.jpg",
+        taille: 178,
+        poids: 80,
+    },
+
+    {
+        name: "Yaya Toure",
+        nationalite: "ivoirien",
+        poste: "defenseur",
+        birth : 1983,
+        picture: "/images/yaya.jpeg",
+        taille: 191,
+        poids: 90,
+    },
+
+    {
+        name: "Gerard Pique",
+        nationalite: "espagnol",
+        poste: "defenseur",
+        birth : 1987,
+        picture: "/images/pique.jpg",
+        taille: 194,
+        poids: 85,
+    },
+
+    {
+        name: "Sylvinho",
+        nationalite: "bresilien",
+        poste: "defenseur",
+        birth : 1974,
+        picture: "/images/sylvinho.jpg",
+        taille: 170,
+        poids: 68,
+    },
+
+    {
+        name: "Victor Valdes",
+        nationalite: "espagnol",
+        poste: "gardien",
+        birth : 1982,
+        picture: "/images/valdes.jpg",
+        taille: 183,
+        poids: 78,
+    },
+];
+
+
+   
+
+
+
+
+
