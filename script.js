42f5c573471260d40726948cd5829723548e682e
otman <otman@DESKTOP-4RA473J.localdomain>
2023-12-13 16:45:14 +0100

    projet V2 by otman

--- script.js
+++ script.js
@@ -3,115 +3,192 @@ const collection=[
     {
         name: "Lionel Messi",
         nationalite:"argentin",
-        poste: "attaquant",
+        poste: "Attaquant",
         birth : 1987,
-        picture: "/images/messi.jpg",
+        picture: "images/messi.jpg",
         taille: 170,
         poids: 72,
+        description:"blabla",
     }, 
     
     {
         name: "Samuel Eto'o",
         nationalite: "camerounais",
-        poste: "attaquant",
+        poste: "Attaquant",
         birth : 1981,
-        picture: "/images/etoo.jpg",
+        picture: "images/etoo.jpg",
         taille: 180,
         poids: 75,
+        description:"blabla",
     },
 
     {
         name: "Thierry Henry",
-        nationalite: "francais",
-        poste: "attaquant",
+        nationalite: "français",
+        poste: "Attaquant",
         birth : 1977,
-        picture: "/images/henry.jpg",
+        picture: "images/henry.jpg",
         taille: 188,
         poids: 83,
+        description:"blabla",
     },
 
     {
         name: "Andres Iniesta",
         nationalite: "espagnol",
-        poste: "milieu",
+        poste: "Milieu",
         birth : 1984,
-        picture: "/images/iniesta.jpg",
+        picture: "images/iniesta.jpg",
         taille: 171,
         poids: 68,
+        description:"blabla",
     },
 
     {
         name: "Xavi",
         nationalite: "espagnol",
-        poste: "milieu",
+        poste: "Milieu",
         birth : 1980,
-        picture: "/images/xavi.jpeg",
+        picture: "images/xavi.jpeg",
         taille: 170,
         poids: 68,
+        description:"blabla",
     },
 
     {
         name: "Sergio Busquets",
         nationalite: "espagnol",
-        poste: "milieu",
+        poste: "Milieu",
         birth : 1980,
-        picture: "/images/sergio.jpg",
+        picture: "images/sergio.jpg",
         taille: 189,
         poids: 76,
+        description:"blabla",
     },
 
     {
         name: "Carles Puyol",
         nationalite: "espagnol",
-        poste: "defenseur",
+        poste: "Defenseur",
         birth : 1978,
-        picture: "/images/puyol.jpg",
+        picture: "images/puyol.jpg",
         taille: 178,
         poids: 80,
+        description:"blabla",
     },
 
     {
         name: "Yaya Toure",
         nationalite: "ivoirien",
-        poste: "defenseur",
+        poste: "Defenseur",
         birth : 1983,
-        picture: "/images/yaya.jpeg",
+        picture: "images/yaya.jpeg",
         taille: 191,
         poids: 90,
+        description:"blabla",
     },
 
     {
         name: "Gerard Pique",
         nationalite: "espagnol",
-        poste: "defenseur",
+        poste: "Defenseur",
         birth : 1987,
-        picture: "/images/pique.jpg",
+        picture: "images/pique.jpg",
         taille: 194,
         poids: 85,
+        description:"blabla",
     },
 
     {
         name: "Sylvinho",
         nationalite: "bresilien",
-        poste: "defenseur",
+        poste: "Defenseur",
         birth : 1974,
-        picture: "/images/sylvinho.jpg",
+        picture: "images/sylvinho.jpg",
         taille: 170,
         poids: 68,
+        description:"blabla",
     },
 
     {
         name: "Victor Valdes",
         nationalite: "espagnol",
-        poste: "gardien",
+        poste: "Gardien",
         birth : 1982,
-        picture: "/images/valdes.jpg",
+        picture: "images/valdes.jpg",
         taille: 183,
         poids: 78,
+        description:"blabla",
     },
 ];
 
+let header = document.querySelector('header');
 
+
+
+document.addEventListener('DOMContentLoaded',function(){
+for(let i=0;i<collection.length;i++){
+
+    let collector=collection[i];
+
+    let main = document.querySelector('main');
+    main.classList.add('container');
+    
+        let section = document.createElement('section');
+        section.classList.add('card');
+
+            let division1 = document.createElement('div');
+            division1.classList.add('card__image');
+
+                let image=document.createElement('img');
+                image.src=collector.picture;
+
+            let article = document.createElement('article');
+            article.classList.add('card__text');
+                
+                let division2= document.createElement('div');
+                division2.classList.add('card__text__natio');
+                division2.innerText=`Nationalité: ${collector.nationalite}`;
+
+                let division3= document.createElement('div');
+                division3.classList.add('card__text__birth');
+                division3.innerText=`Année de naissance: ${collector.birth}`;
+
+                let division4 = document.createElement('div');
+                division4.classList.add('card__text__poste');
+                division4.innerText=`Poste: ${collector.poste}`;
+
+                let division5 = document.createElement('div');
+                division5.classList.add('card__text__taille');
+                division5.innerText=`Taille: ${collector.taille} cm`;
+
+                let division6 = document.createElement('div');
+                division6.classList.add('card__text__poids');
+                division6.innerText=`Poids: ${collector.poids} kg`;
+
+                let nom = document.createElement('h2');
+                nom.classList.add('card__text__nom');
+                nom.innerText=collector.name;
+
+                let paragraph = document.createElement('p');
+                paragraph.classList.add('card__text__paragraph');
+                paragraph.innerText=collector.description;
+
+                
+        main.appendChild(section);
+        section.appendChild(division1);
+        section.appendChild(article);
+        division1.appendChild(image);
+        article.appendChild(nom);
+        article.appendChild(division2);
+        article.appendChild(division3);
+        article.appendChild(division4);
+        article.appendChild(division5);
+        article.appendChild(division6);
+        article.appendChild(paragraph);
+
+    }
+});
    
 
 
